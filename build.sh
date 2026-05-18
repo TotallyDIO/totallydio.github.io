04f7c9e9378a0e28dba1df521261f5ec4c7016ae
TotallyDIO <hunter.tadlock911@gmail.com>
2026-05-17 23:23:41 -0500

    doing stuff

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 PACKAGEDIR=x86_64
 
 # cleans up
-rm PersonalRepo.db PersonalRepo.db.tar.gz PersonalRepo.files PersonalRepo.files.tar.gz
+rm *.db *.tar.gz *.files
 rm -rf pkgbuilds/*/pkg
 rm -rf pkgbuilds/*/src
 rm -rf pkgbuilds/*/*.tar.xz
@@ -12,4 +12,4 @@ ls -R | grep -E PKGBUILD | xargs -I {} sh -c 'cd $(dirname {}) && makepkg -m && 
 ls -R | grep -E *.pkg.tar.zst | xargs -I {} sh -c 'mv {} ../$(basename $(dirname {}))'
 
 # remakes db files
-repo-add PersonalRepo.db.tar.gz ./x86_64/*.pkg.tar.zst
+repo-add totallydio.github.io.db.tar.gz ./x86_64/*.pkg.tar.zst
